cd6cde6d0dc9fa7c1b4cb719ee39f969d3ab8380
Joel Hammer <jhammer@friendsbalt.org>
2024-04-11 12:33:27 -0400

    Added JS fetch code

---
+++ public/scripts/updateProfile.js
@@ -0,0 +1,26 @@
+/**
+ * Updates the user profile with the provided information.
+ *
+ * @param {string} route - The route to send the update request to.
+ * @param {string} firstName - The user's first name.
+ * @param {string} lastName - The user's last name.
+ * @param {string} email - The user's email address.
+ * @param {string} password - The user's password.
+ * @returns {Promise<Response>} - A Promise that resolves to the response of the update request.
+ */
+function updateProfile(route, firstName, lastName, email, password) {
+    const data = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+    };
+
+    return fetch(route, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+}
