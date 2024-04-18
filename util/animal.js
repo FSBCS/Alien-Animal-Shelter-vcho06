00d806d17c6d4790c99cc6c573b634035b469da1
Joel Hammer <jhammer@friendsbalt.org>
2024-04-18 09:17:15 -0400

    update field name

--- util/animal.js
+++ util/animal.js
@@ -9,13 +9,13 @@ class Animal {
      * @param {string} name - The name of the animal.
      * @param {string} description - The description of the animal.
      * @param {string} species - The species of the animal.
-     * @param {string} profilePhotoUri - The URI of the animal's profile photo.
+     * @param {string} photoLocation - The URI of the animal's profile photo.
      */
-    constructor(name, description, species, profilePhotoUri) {
+    constructor(name, description, species, photoLocation) {
         this.name = name;
         this.description = description;
         this.species = species;
-        this.profilePhotoUri = profilePhotoUri;
+        this.photoLocation = photoLocation;
         this.id = null;
     }
 
