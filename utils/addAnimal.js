4a11127404ec666bec4fe785bac229f7bd693fce
vcho06 <113116104+vcho06@users.noreply.github.com>
2024-04-25 19:26:43 +0000

    animal added to db

--- utils/addAnimal.js
+++ utils/addAnimal.js
@@ -1,15 +1,6 @@
 const Animal = require('./animal');
 const DB = require('./db');
 
-const a = new Animal("Fluffy", "A rare martian beaver", "Space Beaver", "/images/spaceBeaver.png");
-DB.insertAnimal(a, (err, animal) => {  
-    if (err) {
-        console.log('Error adding animal: ', err);
-    } else {
-        console.log('Animal added');
-    }
-});
-
 const b = new Animal("Squish", "A squishy turtle alien", "Sea Squish", "/images/turtleSquish.jpg");
 DB.insertAnimal(b, (err, animal) => {  
     if (err) {
